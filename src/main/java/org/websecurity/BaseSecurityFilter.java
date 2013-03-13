e97f5e8512914ef12f6d0ed6a7fa267d8a56fcef
zhwj184 <zhwj184@gmail.com>
2013-03-13 21:03:14 +0800

    new file:   src/main/java/org/websecurity/BaseSecurityFilter.java 	new file:   src/main/java/org/websecurity/SecurityConstant.java 	new file:   src/main/web.xml 	target/classes/org/websecurity/BaseSecurityFilter.class 	target/classes/org/websecurity/CookieWhiteListFilter$CookieWhiteFilterHttpServletRequest.class 	target/classes/org/websecurity/CookieWhiteListFilter$CookieWhiteFilterHttpServletResponse.class 	target/classes/org/websecurity/FileUploadSecurityFilter$UploadFileExtensionFilterHttpServletRequest.class 	target/classes/org/websecurity/SecurityConstant.class 	target/classes/org/websecurity/StaticFilePathSecurityFilter$UriNormalizerHttpServletRequest.class

---
+++ src/main/java/org/websecurity/BaseSecurityFilter.java
@@ -0,0 +1,43 @@
+package org.websecurity;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class BaseSecurityFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain filterChain) throws IOException, ServletException {
+		if (request instanceof HttpServletRequest
+				&& response instanceof HttpServletResponse) {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+			filterChain.doFilter(new SecurityHttpServletRequest(httpRequest), new SecurityHttpServletResponse(httpResponse));
+			return ;
+		}
+		filterChain.doFilter(request, response);
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/org/websecurity/SecurityConstant.java
@@ -0,0 +1,14 @@
+package org.websecurity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecurityConstant {
+	
+	public static final List<String> cookieWhiteList = new ArrayList<String>();
+	
+	public static final List<String> onlyPostUrlList = new ArrayList<String>();
+	
+	public static final List<String> whitefilePostFixList = new ArrayList<String>();
+
+}
